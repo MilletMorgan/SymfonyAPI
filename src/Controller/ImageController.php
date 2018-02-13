f176de5eedaa1d4d0e4c017b21659b7a7a8d6cc5
Ozyhcs <millet.morgan5@gmail.com>
2018-02-13 14:10:02 +0100

    REST API

--- src/Controller/ImageController.php
+++ src/Controller/ImageController.php
@@ -14,6 +14,8 @@ use Doctrine\ORM\Mapping\Annotation;
 use FOS\RestBundle\Controller\FOSRestController;
 use FOS\RestBundle\Controller\Annotations\Get;
 use FOS\RestBundle\Controller\Annotations\Delete;
+use FOS\RestBundle\Controller\Annotations\Post;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -25,95 +27,101 @@ use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Filesystem\Exception\IOExceptionInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
-
-class ImageController extends Controller
+class ImageController extends FOSRestController
 {
-    public function deleteAction(int $id, Request $request)
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
     {
-        // $delete = new Image();
-
-        // $form = $this->createForm(DeleteType::class, $delete);
-        // $form->handleRequest($request);
-
-        // if ($form->isSubmitted()) {
-        //     $files = glob('uploads/images/*'); // get all file names
-        //     foreach ($files as $file) { // iterate files
-        //         if (is_file($file)) {
-        //             unlink($file);
-        //         } // delete file
-        //     }
-
-        //     return $this->redirectToRoute('upload');
-        // }
-
-        // return $this->render('Files/delete.html.twig', array(
-        //     'form' => $form->CreateView()
-        // ));
+        return $this->render('base.html.twig');
     }
-    
-    // public function indexAction()
-    // {
-    //     return $this->render('base.html.twig');
-    // }
-
-    // /**
-    //  * @Get('/upload}')
-    //  */
-    // public function uploadAction(Request $request)
-    // {
-    //     $image = new Image();
-    //     $form = $this->createForm(UploadType::class, $image);
-    //     $form->handleRequest($request);
-
-    //     if ($form->isSubmitted() && $form->isValid()) {
-    //         $em = $this->getDoctrine()->getManager();
-    //         $em->persist($image);
-    //         $em->flush($image);
-
-    //         $file = $image->getImage();
-    //         $fileName = md5(uniqid()).'.'.$file->guessExtension();
-    //         $file->move(
-    //             $this->getParameter('images_directory'),
-    //             $fileName
-    //         );
-
-    //         return $this->redirect($this->generateUrl('download', array('image' => $fileName)));
-    //     }
-
-    //     return $this->render('Files/upload.html.twig', array(
-    //         'image' => $image,
-    //         'form' => $form->CreateView(),
-    //     ));
-    // }
-
-    // /**
-    //  * @Get('/images/{id}')
-    //  */
-    // public function downloadAction(Request $request)
-    // {
-    //     $file = new Image();
-
-    //     $image = $_GET['image'];
-    //     $route = "uploads/images/";
-
-    //     $form = $this->createForm(DownloadType::class, $file);
-    //     $form->handleRequest($request);
-
-    //     if ($form->isSubmitted()) {
-    //         $response = new BinaryFileResponse($route . $image);
-    //         $response->setContentDisposition(ResponseHeaderBag::DISPOSITION_ATTACHMENT);
-            
-    //         return $response;
-    //     }
-
-    //     return $this->render('Files/download.html.twig', array(
-    //         'image' => $image,
-    //         'form' => $form->CreateView(),
-    //     ));
-    // }
 
     /**
-     * @Delete('/images/{id}')
+     * @Route("/upload")
      */
+    public function uploadAction(Request $request)
+    {
+        $image = new Image();
+        $form = $this->createForm(UploadType::class, $image);
+        $form->handleRequest($request);
 
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($image);
+            $em->flush($image);
+
+            $file = $image->getImage();
+            $id = md5(uniqid()).'.'.$file->guessExtension();
+            $file->move(
+                $this->getParameter('images_directory'),
+                $id
+            );
+
+            return $this->redirect($this->generateUrl('download', array('id' => $id)));
+        }
+
+        return $this->render('Files/upload.html.twig', array(
+            'image' => $image,
+            'form' => $form->CreateView(),
+        ));
+    }
+
+    /**
+     * @Route("/download")
+     * @Method({"POST"})
+     */
+    public function downloadAction(Request $request)
+    {
+        $file = new Image();
+
+        $route = "uploads/images/";
+        $id = $_GET['id'];
+
+        $form = $this->createForm(DownloadType::class, $file);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted()) {
+            $response = new BinaryFileResponse($route . $id);
+            $response->setContentDisposition(ResponseHeaderBag::DISPOSITION_ATTACHMENT);
+            
+            return $response;
+        }
+
+        return $this->render('Files/download.html.twig', array(
+            'image' => $id,
+            'form' => $form->CreateView(),
+        ));
+    }
+
+    /**
+     * @Get("/delete")
+     * @Method({"POST"})
+     */
+    public function deleteAction(Request $request)
+    {
+        $delete = new Image();
+
+        $route = "uploads/images/";
+        $id = $_GET['id'];
+
+        $form = $this->createForm(DeleteType::class, $delete);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted()) {
+            $files = glob($route . $id);
+            
+            foreach ($files as $file) {
+                if (is_file($file)) {
+                    unlink($file);
+                }
+            }
+
+            return $this->redirectToRoute('upload');
+        }
+
+        return $this->render('Files/delete.html.twig', array(
+            'form' => $form->CreateView()
+        ));
+    }
 }
